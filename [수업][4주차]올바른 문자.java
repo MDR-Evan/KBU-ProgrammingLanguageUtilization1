dcc43ba895fb8f3b8e3d2167428733571c9d1e7c
Jinyong <135928466+MDR-Evan@users.noreply.github.com>
2023-09-20 12:40:10 +0900

    Add files via upload

---
+++ [수업][4주차]올바른 문자.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        char ch;
+        String result;
+
+        System.out.printf("하나의 문자 입력 : ");
+        ch = keyboard.next().charAt(0);
+
+        result = ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9' ? "올바른 문자 입니다" : "올바르지 않은 문자 입니다.";
+
+        System.out.printf("입력 받은 문자 %c는 %s\n",ch,result);
+    }
+}
