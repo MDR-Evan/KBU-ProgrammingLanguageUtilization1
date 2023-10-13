f6a66013ab013639241a4ab466b6fcdbc49069af
Jinyong <kjy040216@kbu.ac.kr>
2023-10-14 01:03:03 +0900

    update

---
+++ [과제]/[7주차]/week7_Report1_1.java
@@ -0,0 +1,16 @@
+/* 닭과 개의 합은 37마리이고, 다리의 합은 102개다. 닭과 개는 각각 몇 마리인지를 알아내는 프로그램을 for문, while문, do-while문으로 작성하시오. */
+public  class week7_Report1_1 {
+    public static void main(String[] args) {
+        /** x + y = 37 : 2x + 4y = 102 **/
+        final int totalanimal = 37, totalanimal_reg = 102;
+        int chicken, dog;
+
+        for (chicken = 0; chicken <= totalanimal; chicken++) {
+            for (dog = 1; dog <= totalanimal; dog++) {
+                if (chicken + dog == totalanimal && (chicken * 2) + (dog * 4) == totalanimal_reg) {
+                    System.out.printf("닭 : %d마리\n개 : %d마리\n", chicken, dog);
+                }
+            }
+        }
+    }
+}
---
+++ [과제]/[7주차]/week7_Report1_3.java
@@ -0,0 +1,18 @@
+/* 닭과 개의 합은 37마리이고, 다리의 합은 102개다. 닭과 개는 각각 몇 마리인지를 알아내는 프로그램을 for문, while문, do-while문으로 작성하시오. */
+public class week7_Report1_3 {
+    public static void main(String[] args) {
+        /** x + y = 37 : 2x + 4y = 102 **/
+        final int totalanimal = 37, totalanimal_reg = 102;
+        int chicken = 0, dog = 1;
+
+        do {
+            if (chicken + dog == totalanimal && (chicken * 2) + (dog * 4) == totalanimal_reg) {
+                break;
+            } else {
+                chicken++;
+                dog = totalanimal - chicken;
+            }
+        } while (chicken <= totalanimal);
+        System.out.printf("닭 : %d마리\n개 : %d마리\n", chicken, dog);
+    }
+}
