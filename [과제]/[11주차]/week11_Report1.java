1307d854446f52cffbc62541f62cfedb5157562b
Jinyong <kjy040216@kbu.ac.kr>
2023-11-14 21:39:44 +0900

    update

--- [과제]/[11주차]/week11_Report1.java
+++ [과제]/[11주차]/week11_Report1.java
@@ -20,90 +20,90 @@ public class week11_Report1 {
         };// 부서코드 | 보훈(원호) | 급 | 호 | 공제액 | 기본공제
         int i = 0;
         // 입력
-        for (i = 0; i < person.length; i++) {
-            System.out.println("==================================================================");
-            // 이름
-            System.out.printf("사원 %d번의 이름을 입력하세요 : ", i + 1);
-            person[i][1] = keyboard.nextLine();
-            person[i][0] = null;
-            data[i][0] = 0;
-            // 사번
-            System.out.printf("%s 사원의 사번을 입력하세요 : ", person[i][1]);
-            while (person[i][0] == null) {
-                String ID = keyboard.nextLine();
-                if (ID.length() == 4) {
-                    person[i][0] = ID;
-                } else {
-                    System.err.print("사번은 4자여야 합니다. 다시 입력해주세요 : ");
-                }
-            }
-            // 부서코드
-            System.out.printf("%s 사원의 부서코드를 입력하세요 : ", person[i][1]);
-            while (true) {
-                int department = keyboard.nextInt();
-
-                if (0 < department && department < 6) {
-                    data[i][0] = department;
-                    break;
-                } else {
-                    System.err.print("부서코드는 [ 1 경리팀, 2 인사팀, 3 영업팀, 4 생산팀, 5 서비스 ]입니다. 다시 입력해주세요 : ");
-                    keyboard.nextLine();
-                }
-            }
-            // 보훈
-            System.out.printf("%s 사원의 보훈의 여부를 입력하세요 ('O' = 1, 'X' = 0): ", person[i][1]);
-            while (true) {
-                int bohun = keyboard.nextInt();
-
-                if (0 == bohun || 1 == bohun) {
-                    data[i][1] = bohun;
-                    break;
-                } else {
-                    System.err.print("보훈대상자 구분은 [ 보훈대상자가 아니면 '0' 보훈대상자가 맞으면 '1' ]입니다. 다시 입력해주세요 : ");
-                    keyboard.nextLine();
-                }
-            }
-            // 직급
-            System.out.printf("%s 사원의 직급을 입력하세요 : ", person[i][1]);
-            while (true) {
-                int grade = keyboard.nextInt();
-
-                if (0 < grade && grade < 4) {
-                    data[i][2] = grade;
-                    break;
-                } else {
-                    System.err.print("직급은 [ 1 ~ 3급 ]입니다. 다시 입력해주세요 : ");
-                    keyboard.nextLine();
-                }
-            }
-            // 호봉
-            System.out.printf("%s 사원의 호봉을 입력하세요 : ", person[i][1]);
-            while (true) {
-                int clas = keyboard.nextInt();
-
-                if (0 < clas && clas < 6) {
-                    data[i][3] = clas;
-                    break;
-                } else {
-                    System.err.print("호봉은 [ 1 ~ 5호봉 ]입니다. 다시 입력해주세요 : ");
-                    keyboard.nextLine();
-                }
-            }
-            // 공제액
-            System.out.printf("%s 사원의 공제액을 입력하세요 : ", person[i][1]);
-            while (true) {
-                int benefit = keyboard.nextInt();
-
-                if (0 <= benefit && benefit <= 300000) {
-                    data[i][4] = benefit;
-                    break;
-                } else {
-                    System.err.print("공제액은 [ 최대 300,000원 ]까지 저축 가능합니다. 다시 입력해주세요 : ");
-                    keyboard.nextLine();
-                }
-            }
-            keyboard.nextLine();
-        }
+//        for (i = 0; i < person.length; i++) {
+//            System.out.println("==================================================================");
+//            // 이름
+//            System.out.printf("사원 %d번의 이름을 입력하세요 : ", i + 1);
+//            person[i][1] = keyboard.nextLine();
+//            person[i][0] = null;
+//            data[i][0] = 0;
+//            // 사번
+//            System.out.printf("%s 사원의 사번을 입력하세요 : ", person[i][1]);
+//            while (person[i][0] == null) {
+//                String ID = keyboard.nextLine();
+//                if (ID.length() == 4) {
+//                    person[i][0] = ID;
+//                } else {
+//                    System.err.print("사번은 4자여야 합니다. 다시 입력해주세요 : ");
+//                }
+//            }
+//            // 부서코드
+//            System.out.printf("%s 사원의 부서코드를 입력하세요 : ", person[i][1]);
+//            while (true) {
+//                int department = keyboard.nextInt();
+//
+//                if (0 < department && department < 6) {
+//                    data[i][0] = department;
+//                    break;
+//                } else {
+//                    System.err.print("부서코드는 [ 1 경리팀, 2 인사팀, 3 영업팀, 4 생산팀, 5 서비스 ]입니다. 다시 입력해주세요 : ");
+//                    keyboard.nextLine();
+//                }
+//            }
+//            // 보훈
+//            System.out.printf("%s 사원의 보훈의 여부를 입력하세요 ('O' = 1, 'X' = 0): ", person[i][1]);
+//            while (true) {
+//                int bohun = keyboard.nextInt();
+//
+//                if (0 == bohun || 1 == bohun) {
+//                    data[i][1] = bohun;
+//                    break;
+//                } else {
+//                    System.err.print("보훈대상자 구분은 [ 보훈대상자가 아니면 '0' 보훈대상자가 맞으면 '1' ]입니다. 다시 입력해주세요 : ");
+//                    keyboard.nextLine();
+//                }
+//            }
+//            // 직급
+//            System.out.printf("%s 사원의 직급을 입력하세요 : ", person[i][1]);
+//            while (true) {
+//                int grade = keyboard.nextInt();
+//
+//                if (0 < grade && grade < 4) {
+//                    data[i][2] = grade;
+//                    break;
+//                } else {
+//                    System.err.print("직급은 [ 1 ~ 3급 ]입니다. 다시 입력해주세요 : ");
+//                    keyboard.nextLine();
+//                }
+//            }
+//            // 호봉
+//            System.out.printf("%s 사원의 호봉을 입력하세요 : ", person[i][1]);
+//            while (true) {
+//                int clas = keyboard.nextInt();
+//
+//                if (0 < clas && clas < 6) {
+//                    data[i][3] = clas;
+//                    break;
+//                } else {
+//                    System.err.print("호봉은 [ 1 ~ 5호봉 ]입니다. 다시 입력해주세요 : ");
+//                    keyboard.nextLine();
+//                }
+//            }
+//            // 공제액
+//            System.out.printf("%s 사원의 공제액을 입력하세요 : ", person[i][1]);
+//            while (true) {
+//                int benefit = keyboard.nextInt();
+//
+//                if (0 <= benefit && benefit <= 300000) {
+//                    data[i][4] = benefit;
+//                    break;
+//                } else {
+//                    System.err.print("공제액은 [ 최대 300,000원 ]까지 저축 가능합니다. 다시 입력해주세요 : ");
+//                    keyboard.nextLine();
+//                }
+//            }
+//            keyboard.nextLine();
+//        }
 
         // 부서코드 -> 부서
         String[] department = new String[person.length];
@@ -248,7 +248,7 @@ public class week11_Report1 {
                     person[k][1] = person[k + 1][1];
                     person[k + 1][1] = tempPersonNAME;
 
-                    // 직급, 호봉, 보훈, 본봉 교환
+                    // 직급, 호봉, 본봉 교환
                     int tempGrade = data[k][2];
                     data[k][2] = data[k + 1][2];
                     data[k + 1][2] = tempGrade;
@@ -257,15 +257,11 @@ public class week11_Report1 {
                     data[k][3] = data[k + 1][3];
                     data[k + 1][3] = tempClas;
 
-                    int tempBohun = data[k][1];
-                    data[k][1] = data[k + 1][1];
-                    data[k + 1][1] = tempBohun;
-
                     int tempSalary = salary[k];
                     salary[k] = salary[k + 1];
                     salary[k + 1] = tempSalary;
 
-                    // 직급수당, 업무수당, 개인공제, 세금, 지급액 교환
+                    // 직급수당, 업무수당, 개인공제, 세금, 지급액, 보훈 교환
                     int tempGradeSalary = grade_salary[k];
                     grade_salary[k] = grade_salary[k + 1];
                     grade_salary[k + 1] = tempGradeSalary;
@@ -281,6 +277,10 @@ public class week11_Report1 {
                     int tempTax = tax[k];
                     tax[k] = tax[k + 1];
                     tax[k + 1] = tempTax;
+
+                    String tempBohun = bohun[k];
+                    bohun[k] = bohun[k + 1];
+                    bohun[k + 1] = tempBohun;
                 }
             }
         }
