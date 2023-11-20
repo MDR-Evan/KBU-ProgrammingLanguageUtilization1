920224ba362f5ce682aceada9cc970ca317d839b
Jinyong <135928466+MDR-Evan@users.noreply.github.com>
2023-11-20 16:08:46 +0900

    update

---
+++ [과제]/[12주차]/week12_Report3.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class week12_Report3 {
+    private static String convert() {
+        String[] number = {"일", "이", "삼", "사", "오", "육", "칠", "팔", "구"};
+        String[]
+    }
+}
