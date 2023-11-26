c10678830285dbc8569f34199a26fd36a762650c
Jinyong <kjy040216@kbu.ac.kr>
2023-11-26 16:10:40 +0900

    update

--- [과제]/[13주차]/week13_Report1_3.java
+++ [과제]/[13주차]/week13_Report1_3.java
@@ -1,22 +1,35 @@
-//public class week13_Report1_3 {
-//    public static void main(String[] args) {
-//        int[] data = {};
-//        data = triple();
-//
-//        print(data, "******* Source data *******");
-//
-//
-//        for (int i = 0; i < data.length; i++) {
-//            for (int j = 0; j < data[i].length; j++) {
-//                triple(data[i][j]);
-//            }
-//        }
-//
-//
-//        print(data, "\n******* Triple data *******");
-//    }
-//
-//    private static int[] triple() {
-//        int[][] data = {{2, 4, 5}, {8, 7, 6}, {3, 5, 6}, {9, 12, 10}};
-//    }
-//}
+public class week13_Report1_3 {
+    public static void main(String[] args) {
+        int[] data;
+        data = triple();
+
+        print(data, "******* Source data *******");
+
+
+        for (int i = 0; i < data.length; i++) {
+            triple(data[i]);
+        }
+
+
+        print(data, "\n******* Triple data *******");
+    }
+
+    private static int[] triple() {
+        int[][] data = {{2, 4, 5}, {8, 7, 6}, {3, 5, 6}, {9, 12, 10}};
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                data[i][j] *= 3;
+            }
+        }
+    }
+
+    private static void print(int[][] data, String s) {
+        System.out.printf("%s\n",s);
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%4d, ",data[i]);
+            System.out.printf("\b\b");
+            System.out.println();
+        }
+    }
+}
